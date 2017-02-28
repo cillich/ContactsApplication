13c91e08bd79f391a6c68bc19f0778a6e077e244
Claire Illich <claireillich@comcast.net>
2017-02-28 12:38:46 -0500

    Connected mongo database to ContactsApp in order to get contacts and removed dummy data

--- server.js
+++ server.js
@@ -1,32 +1,16 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contactlist', function(req, res) {
     console.log("I received a GET request");
-
-    // Dummy data
-    person1 = {
-        name: 'Claire',
-        email: 'claire@example.com',
-        number: '(111) 111-1111'
-    };
-
-    person2 = {
-        name: 'George',
-        email: 'george@gmail.com',
-        number: '(111) 222-2222'
-    };
-
-    person3 = {
-        name: 'Bill',
-        email: 'bill@example.com',
-        number: '(111) 333-3333'
-    };
-
-    var contactlist = [person1, person2, person3];
-    res.json(contactlist);
+    db.contactlist.find(function (err, docs){
+        console.log(docs);
+        res.json(docs);
+    })
 });
 
 app.listen(3000);
