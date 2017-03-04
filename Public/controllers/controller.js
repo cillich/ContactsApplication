5755f2d8241e6532991d6539e216577c6160d5ce
Claire Illich <claireillich@comcast.net>
2017-03-03 19:23:34 -0500

    Added edit UI and functionality to edit contacts in the database. Also added a clear button to clear contact info from text boxes.

--- Public/controllers/controller.js
+++ Public/controllers/controller.js
@@ -5,12 +5,10 @@ angular.module('myApp', []).controller('AppCtrl', function($scope, $http) {
     var refresh = function(){
         $http({
             method: 'GET',
-            url: '/contactlist'
+            url: '/contactlist/'
         }).then(function(response){
             $scope.contactlist = response.data;
-            $scope.contact.name = "";
-            $scope.contact.email = "";
-            $scope.contact.number = "";
+            clearContact();
         });
     };
 
@@ -18,7 +16,7 @@ angular.module('myApp', []).controller('AppCtrl', function($scope, $http) {
 
     $scope.addContact = function() {
         console.log($scope.contact);
-        $http.post('/contactlist', $scope.contact).then(function(response){
+        $http.post('/contactlist/', $scope.contact).then(function(response){
             console.log(response);
             refresh();
         });
@@ -31,7 +29,33 @@ angular.module('myApp', []).controller('AppCtrl', function($scope, $http) {
         .then(function(response) {
             refresh();
         });
+    };
 
+    $scope.edit = function(id) {
+        console.log(id);
+        $http.get('/contactlist/' + id)
+        .then(function(response){
+            console.log(response.data);
+            $scope.contact = response.data;
+        });
+    };
+
+    $scope.update = function() {
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact)
+        .then(function(response) {
+            refresh();
+        })
+    };
+
+    $scope.deselect = function() {
+        clearContact();
+    }
+
+    clearContact = function() {
+        $scope.contact.name = "";
+        $scope.contact.email = "";
+        $scope.contact.number = "";
     };
 
 
